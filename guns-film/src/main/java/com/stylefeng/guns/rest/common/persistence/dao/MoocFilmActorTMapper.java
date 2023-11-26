8bcf49027c0a94b7f7ec86b05b2622edd5eb7163
CadeYu <yu83612457@gmail.com>
2023-11-26 21:47:23 +0800

    feat(用户模块):film业务代码

---
+++ guns-film/src/main/java/com/stylefeng/guns/rest/common/persistence/dao/MoocFilmActorTMapper.java
@@ -0,0 +1,16 @@
+package com.stylefeng.guns.rest.common.persistence.dao;
+
+import com.stylefeng.guns.rest.common.persistence.model.MoocFilmActorT;
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 影片与演员映射表 Mapper 接口
+ * </p>
+ *
+ * @author cade
+ * @since 2023-11-26
+ */
+public interface MoocFilmActorTMapper extends BaseMapper<MoocFilmActorT> {
+
+}
--- guns-film/src/main/java/com/stylefeng/guns/rest/modular/film/service/DefaultFilmServiceImpl.java
+++ guns-film/src/main/java/com/stylefeng/guns/rest/modular/film/service/DefaultFilmServiceImpl.java
@@ -1,12 +1,18 @@
 package com.stylefeng.guns.rest.modular.film.service;
 
 import com.alibaba.dubbo.config.annotation.Service;
+import com.baomidou.mybatisplus.mapper.EntityWrapper;
+import com.baomidou.mybatisplus.plugins.Page;
 import com.stylefeng.guns.api.film.FilmServiceApi;
 import com.stylefeng.guns.api.film.vo.BannerVO;
 import com.stylefeng.guns.api.film.vo.FilmInfo;
 import com.stylefeng.guns.api.film.vo.FilmVO;
+import com.stylefeng.guns.core.util.DateUtil;
 import com.stylefeng.guns.rest.common.persistence.dao.MoocBannerTMapper;
+import com.stylefeng.guns.rest.common.persistence.dao.MoocFilmInfoTMapper;
+import com.stylefeng.guns.rest.common.persistence.dao.MoocFilmTMapper;
 import com.stylefeng.guns.rest.common.persistence.model.MoocBannerT;
+import com.stylefeng.guns.rest.common.persistence.model.MoocFilmT;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -19,6 +25,10 @@ import java.util.List;
 public class DefaultFilmServiceImpl implements FilmServiceApi {
     @Autowired
     private  MoocBannerTMapper moocBannerTMapper;
+    @Autowired
+    private  MoocFilmTMapper moocFilmTMapper;
+
+
     @Override
     public List<BannerVO> getBanners() {
         List<BannerVO> result = new ArrayList<BannerVO>();
@@ -33,28 +43,102 @@ public class DefaultFilmServiceImpl implements FilmServiceApi {
         return result;
     }
 
+    private List<FilmInfo> getFilmInfos(List<MoocFilmT> moocFilmT) {
+        List<FilmInfo> res = new ArrayList<FilmInfo>();
+        for (MoocFilmT filmT : moocFilmT) {
+            FilmInfo filmInfo = new FilmInfo();
+            filmInfo.setFilmName(filmT.getFilmName());
+            filmInfo.setFilmType(filmT.getFilmType());
+            filmInfo.setFilmScore(filmT.getFilmScore());
+            filmInfo.setFilmId(filmT.getUuid()+"");
+            filmInfo.setImgAddress(filmT.getImgAddress());
+            filmInfo.setExpectNum(String.valueOf(filmT.getFilmPresalenum()));
+            filmInfo.setBoxNum(filmT.getFilmBoxOffice());
+            filmInfo.setScore(filmT.getFilmScore());
+            filmInfo.setShowTime(DateUtil.getDay(filmT.getFilmTime()));
+            res.add(filmInfo);
+        }
+        return res;
+    }
+
     @Override
     public FilmVO getHotFilms(boolean isLimited, int number) {
-        return null;
+        FilmVO filmVO = new FilmVO();
+        List<FilmInfo> filmInfos = new ArrayList<FilmInfo>();
+
+        //限制影片为热映状态
+        EntityWrapper<MoocFilmT> entityWrapper = new EntityWrapper<>();
+        entityWrapper.eq("film_status",1);
+        if (isLimited){
+            //判断是否为首页需要的内容
+            //1.是，则限制展示的条数
+            Page<MoocFilmT> page = new Page<>(1,number);
+            List<MoocFilmT> moocFilmTS = moocFilmTMapper.selectPage(page, entityWrapper);
+            //封装FilmInfo
+            List<FilmInfo> filmInfoList = getFilmInfos(moocFilmTS);
+            filmVO.setFilmInfos(filmInfoList);
+            filmVO.setFilmNum(moocFilmTS.size());
+        }else {
+            //2.不是，则是列表页
+        }
+        return filmVO;
+
     }
 
     @Override
     public FilmVO getUpcomingFilms(boolean isLimited, int number) {
-        return null;
+        FilmVO filmVO = new FilmVO();
+        List<FilmInfo> filmInfos = new ArrayList<FilmInfo>();
+
+        //限制影片为即将上映状态
+        EntityWrapper<MoocFilmT> entityWrapper = new EntityWrapper<>();
+        entityWrapper.eq("film_status",2);
+        if (isLimited){
+            //判断是否为首页需要的内容
+            //1.是，则限制展示的条数
+            Page<MoocFilmT> page = new Page<>(1,number);
+            List<MoocFilmT> moocFilmTS = moocFilmTMapper.selectPage(page, entityWrapper);
+            //封装FilmInfo
+            List<FilmInfo> filmInfoList = getFilmInfos(moocFilmTS);
+            filmVO.setFilmInfos(filmInfoList);
+            filmVO.setFilmNum(moocFilmTS.size());
+        }else {
+            //2.不是，则是列表页
+        }
+        return filmVO;
     }
 
     @Override
     public List<FilmInfo> getBoxRanking() {
-        return null;
+        //票房排名前10，正在上映的
+        EntityWrapper<MoocFilmT> entityWrapper = new EntityWrapper<>();
+        entityWrapper.eq("film_status",1);
+        Page<MoocFilmT> page = new Page<>(1,10,"film_box_office");
+        List<MoocFilmT> moocFilmTS = moocFilmTMapper.selectPage(page, entityWrapper);
+        List<FilmInfo> filmInfoList = getFilmInfos(moocFilmTS);
+        return filmInfoList;
     }
 
     @Override
     public List<FilmInfo> getExpectRanking() {
-        return null;
+        //预售票房排名前10，即将上映的
+
+        EntityWrapper<MoocFilmT> entityWrapper = new EntityWrapper<>();
+        entityWrapper.eq("film_status",2);
+        Page<MoocFilmT> page = new Page<>(1,10,"film_preSaleNum");
+        List<MoocFilmT> moocFilmTS = moocFilmTMapper.selectPage(page, entityWrapper);
+        List<FilmInfo> filmInfoList = getFilmInfos(moocFilmTS);
+        return filmInfoList;
     }
 
     @Override
     public List<FilmInfo> getTop() {
-        return null;
+        //正在上映的，评分前10
+        EntityWrapper<MoocFilmT> entityWrapper = new EntityWrapper<>();
+        entityWrapper.eq("film_status",1);
+        Page<MoocFilmT> page = new Page<>(1,10,"film_score");
+        List<MoocFilmT> moocFilmTS = moocFilmTMapper.selectPage(page, entityWrapper);
+        List<FilmInfo> filmInfoList = getFilmInfos(moocFilmTS);
+        return filmInfoList;
     }
 }
